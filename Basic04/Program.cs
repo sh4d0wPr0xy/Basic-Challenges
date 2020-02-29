1b84dc60c3c1978f0b77e0a9cec6bdb7c93d0599
Sh4d0wpr0xy <rarihestalt@aol.com>
2020-02-29 13:54:32 +0100

    Done number 5

---
+++ Basic04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Basic04
+{
+    class Program
+    {
+        /// <summary>
+        /// Write a C# Sharp program to print the result of the specified operations.
+        /// -1 + 4 * 6
+        /// ( 35+ 5 ) % 7
+        /// 14 + -4 * 6 / 11
+        /// 2 + 15 / 6 * 1 - 7 % 2 
+        /// </summary>
+        static void Main()
+        {
+            // Equation 1
+            Console.WriteLine(-1 + 4 * 6);
+
+            // Equation 2
+            Console.WriteLine((35 + 5) % 7);
+
+            // Equation 3
+            Console.WriteLine(14 + -4 * 6 / 11);
+
+            // Equation 4
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+        }
+    }
+}
---
+++ Basic05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Basic05
+{
+    class Program
+    {
+        /// <summary>
+        /// Write a C# Sharp program to swap two numbers.
+        /// Test Data:
+        /// Input the First Number : 5
+        /// Input the Second Number : 6
+        /// Expected Output:
+        /// After Swapping :
+        /// First Number : 6
+        /// Second Number : 5 
+        /// </summary>
+        static void Main()
+        {
+            double firstNumber, secondNumber, temporaryNumber;
+
+            Console.Write("Input the first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Input the second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            // Swap numbers
+            temporaryNumber = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temporaryNumber;
+
+            Console.WriteLine($"Numbers after the swap:\n" +
+                $"First number: {firstNumber}\n" +
+                $"Second number: {secondNumber}");    
+        }
+    }
+}
