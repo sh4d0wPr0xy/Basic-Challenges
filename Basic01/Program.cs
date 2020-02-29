9ef6076ec6df97c52c5c788c86bff66ad31a08d5
Sh4d0wpr0xy <rarihestalt@aol.com>
2020-02-29 13:20:28 +0100

    First one completed.

---
+++ Basic01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Basic01
+{
+    class Program
+    {
+        /// <summary>
+        /// Write a C# Sharp program to print Hello and your name in a separate line.
+        /// </summary>
+        static void Main()
+        {
+            string myName = "Shadow Proxy";
+            Console.WriteLine($"Hello\n{myName}");
+        }
+    }
+}
