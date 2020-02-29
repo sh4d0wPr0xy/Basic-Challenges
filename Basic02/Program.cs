0917ba327e6b0b23bc18488e39875837778d1ec1
Sh4d0wpr0xy <rarihestalt@aol.com>
2020-02-29 13:24:31 +0100

    Completed the first 3 challenges

---
+++ Basic02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Basic02
+{
+    class Program
+    {
+        /// <summary>
+        ///  Write a C# Sharp program to print the sum of two numbers.
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine(1+1);
+        }
+    }
+}
---
+++ Basic03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Basic03
+{
+    class Program
+    {
+        /// <summary>
+        /// Write a C# Sharp program to print the result of dividing two numbers.
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine(1-1);
+        }
+    }
+}
